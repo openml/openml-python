1cbd06f37bf189d1879ef9cd62a400e8de0d32ff
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2016-04-21 11:28:43 +0200

    MAINT remove unused exceptions

--- openml/exceptions.py
+++ openml/exceptions.py
@@ -1,13 +1,3 @@
-class OpenMLStatusChange(Warning):
-    def __init__(self, message):
-        super(OpenMLStatusChange, self).__init__(message)
-
-
-class OpenMLDatasetStatusChange(OpenMLStatusChange):
-    def __init__(self, message):
-        super(OpenMLDatasetStatusChange, self).__init__(message)
-
-
 class PyOpenMLError(Exception):
     def __init__(self, message):
         super(PyOpenMLError, self).__init__(message)
