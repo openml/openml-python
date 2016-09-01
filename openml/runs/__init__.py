2f334e28e7ef7ddc2f1050f8fe98c76dd69ab5d5
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2016-09-01 15:16:38 +0200

    MAINT split task and flow files into separate files

--- openml/runs/__init__.py
+++ openml/runs/__init__.py
@@ -1,7 +1,7 @@
 from .run import OpenMLRun
-from .run import (run_task, get_run, list_runs, list_runs_by_flow,
-                  list_runs_by_tag, list_runs_by_task, list_runs_by_uploader,
-                  list_runs_by_filters)
+from .functions import (run_task, get_run, list_runs, list_runs_by_flow,
+                        list_runs_by_tag, list_runs_by_task,
+                        list_runs_by_uploader, list_runs_by_filters)
 
 __all__ = ['OpenMLRun', 'run_task', 'get_run', 'list_runs', 'list_runs_by_flow',
            'list_runs_by_tag', 'list_runs_by_task', 'list_runs_by_uploader',
