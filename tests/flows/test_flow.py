05b25801e95742b39c2433d7e896f3fd1ce75543
Andreas Mueller <amueller@nyu.edu>
2016-10-20 18:11:07 -0400

    remove mocking from flow test

--- tests/flows/test_flow.py
+++ tests/flows/test_flow.py
@@ -1,13 +1,13 @@
 import collections
 import hashlib
 import re
-import sys
 import time
 import unittest
 
 import xmltodict
 
 import scipy.stats
+import sklearn
 import sklearn.datasets
 import sklearn.decomposition
 import sklearn.dummy
@@ -21,11 +21,6 @@ import sklearn.tree
 from openml.testing import TestBase
 from openml._api_calls import _perform_api_call
 import openml
-
-if sys.version_info[0] >= 3:
-    from unittest import mock
-else:
-    import mock
 
 
 class TestFlow(TestBase):
@@ -106,8 +101,7 @@ class TestFlow(TestBase):
         self.assertEqual(new_flow, flow)
         self.assertIsNot(new_flow, flow)
 
-    @mock.patch.object(openml.OpenMLFlow, '_get_name', autospec=True)
-    def test_publish_flow(self, name_mock):
+    def test_publish_flow(self):
         # Create a unique prefix for the flow. Necessary because the flow is
         # identified by its name and external version online. Having a unique
         #  name allows us to publish the same flow in each test run
@@ -126,13 +120,12 @@ class TestFlow(TestBase):
                                  language='English',
                                  dependencies=''
                                  )
-        name_mock.return_value = 'TEST%s%s' % (sentinel, flow.name)
+        flow.name = 'TEST%s%s' % (sentinel, flow.name)
 
         flow.publish()
         self.assertIsInstance(flow.flow_id, int)
 
-    @mock.patch.object(openml.OpenMLFlow, '_get_name', autospec=True)
-    def test_sklearn_to_upload_to_flow(self, name_mock):
+    def test_sklearn_to_upload_to_flow(self):
         iris = sklearn.datasets.load_iris()
         X = iris.data
         y = iris.target
@@ -144,12 +137,6 @@ class TestFlow(TestBase):
         md5.update(str(time.time()).encode('utf-8'))
         sentinel = md5.hexdigest()[:10]
         sentinel = 'TEST%s' % sentinel
-        def side_effect(self):
-            if sentinel in self.name:
-                return self.name
-            else:
-                return '%s%s' % (sentinel, self.name)
-        name_mock.side_effect = side_effect
 
         # Test a more complicated flow
         ohe = sklearn.preprocessing.OneHotEncoder(categorical_features=[1])
@@ -171,6 +158,7 @@ class TestFlow(TestBase):
             estimator=model, param_distributions=parameter_grid, cv=cv)
         rs.fit(X, y)
         flow = openml.flows.sklearn_to_flow(rs)
+        flow.external_version = sentinel + flow.external_version
 
         flow.publish()
         self.assertIsInstance(flow.flow_id, int)
@@ -178,15 +166,6 @@ class TestFlow(TestBase):
         # Check whether we can load the flow again
         # Remove the sentinel from the name again so that we can reinstantiate
         # the object again
-        def side_effect(self):
-            if sentinel in self.name:
-                name = self.name.replace(sentinel, '')
-                return name
-            else:
-                return self.name
-        name_mock.side_effect = side_effect
-
-        name_mock.side_effect = side_effect
         new_flow = openml.flows.get_flow(flow_id=flow.flow_id)
 
         local_xml = flow._to_xml()
@@ -205,12 +184,10 @@ class TestFlow(TestBase):
             server_xml = server_xml.replace('  ', '').replace('\t', '').strip().replace('\n\n', '\n').replace('&quot;', '"')
             server_xml = re.sub(r'^$', '', server_xml)
 
-
         self.assertEqual(server_xml, local_xml)
 
         self.assertEqual(new_flow, flow)
         self.assertIsNot(new_flow, flow)
-        new_flow.model.fit(X, y)
 
         fixture_name = 'sklearn.model_selection._search.RandomizedSearchCV(' \
                        'sklearn.model_selection._split.StratifiedKFold,' \
@@ -223,5 +200,6 @@ class TestFlow(TestBase):
                        'sklearn.ensemble.weight_boosting.AdaBoostClassifier(' \
                        'sklearn.tree.tree.DecisionTreeClassifier)))'
 
-        self.assertEqual(new_flow._get_name(), fixture_name)
+        self.assertEqual(new_flow.name, fixture_name)
 
+        new_flow.model.fit(X, y)
