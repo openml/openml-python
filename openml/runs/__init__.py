d925655f9650ce326eadebe210078669ddd16462
janvanrijn <janvanrijn@gmail.com>
2017-03-03 15:31:03 +0100

    if else around non guaranteed run element

--- openml/runs/__init__.py
+++ openml/runs/__init__.py
@@ -1,4 +1,4 @@
 from .run import OpenMLRun
-from .functions import (run_task, get_run, list_runs)
+from .functions import (run_task, get_run, list_runs, get_runs)
 
-__all__ = ['OpenMLRun', 'run_task', 'get_run', 'list_runs']
+__all__ = ['OpenMLRun', 'run_task', 'get_run', 'list_runs', 'get_runs']
