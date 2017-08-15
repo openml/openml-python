c80c295307e966fd83b6632390b7ec7ebe3fa3e4
Jan van Rijn <janvanrijn@gmail.com>
2017-08-15 11:05:32 +0200

    fixed code for arxiv paper

--- tests/test_study/test_study_functions.py
+++ tests/test_study/test_study_functions.py
@@ -14,3 +14,12 @@ class TestStudyFunctions(TestBase):
         self.assertEquals(len(study.tasks), 105)
         self.assertEquals(len(study.flows), 27)
         self.assertEquals(len(study.setups), 30)
+
+    def test_get_tasks(self):
+        study_id = 14
+
+        study = openml.study.get_study(study_id, 'tasks')
+        self.assertEquals(study.data, None)
+        self.assertGreater(len(study.tasks), 0)
+        self.assertEquals(study.flows, None)
+        self.assertEquals(study.setups, None)
