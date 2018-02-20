e4035c193e9ceb287726399d7eb4bb20631d5fba
Joaquin Vanschoren <joaquin.vanschoren@gmail.com>
2018-02-20 08:48:15 +0100

    fix for list_all. It currently keeps looping forever (until the connection breaks) (#390)

--- openml/utils.py
+++ openml/utils.py
@@ -118,9 +118,10 @@ def list_all(listing_call, batch_size=10000, *args, **filters):
     dict
     """
     page = 0
+    has_more = 1
     result = {}
 
-    while True:
+    while has_more:
         try:
             new_batch = listing_call(
                 *args,
@@ -135,5 +136,6 @@ def list_all(listing_call, batch_size=10000, *args, **filters):
                 break
         result.update(new_batch)
         page += 1
+        has_more = (len(new_batch) == batch_size)
 
     return result
