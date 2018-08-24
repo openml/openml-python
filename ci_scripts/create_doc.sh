dc3efd2afa12bf1de13eeb2a3997c5009ab20d92
ArlindKadra <arlindkadra@gmail.com>
2018-08-24 15:03:28 +0200

    Initial commit

---
+++ ci_scripts/create_doc.sh
@@ -0,0 +1,17 @@
+set -o pipefail
+
+# delete any previous documentation folder for the branch
+# if it exists
+if [-d ../doc/$1 ]; then
+    rm -rf ../doc/$1
+fi
+
+
+# create the documentation
+cd ../doc && make html 2>&1 | tee ~/log.txt
+
+# create directory with branch name
+mkdir $1
+
+# copy content
+cp - r build/html/* $1
