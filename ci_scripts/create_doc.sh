826cb9340b4294fc836b4e508a3a5f740bd0d94b
ArlindKadra <arlindkadra@gmail.com>
2018-08-24 15:23:19 +0200

    First try at a solution

--- ci_scripts/create_doc.sh
+++ ci_scripts/create_doc.sh
@@ -2,13 +2,13 @@ set -o pipefail
 
 # delete any previous documentation folder for the branch
 # if it exists
-if [-d ../doc/$1 ]; then
-    rm -rf ../doc/$1
+if [ -d doc/$1 ]; then
+    rm -rf doc/$1
 fi
 
 
 # create the documentation
-cd ../doc && make html 2>&1 | tee ~/log.txt
+cd doc && make html 2>&1 | tee ~/log.txt
 
 # create directory with branch name
 mkdir $1
