3af0ecb23c8c9d6ea3a15db7842de52b3d267e7c
Arlind Kadra <arlindkadra@gmail.com>
2018-09-20 17:52:36 +0200

    Pep8 enforce (#535)
    * First initial change
    
    * Changing notation, fixing fetch from master
    
    * [MRG] CI: add flake8 check in travis (#534)
    
    * CI: add flake8 check in travis
    
    * FIX: typo
    
    * Updated accordingly

--- ci_scripts/test.sh
+++ ci_scripts/test.sh
@@ -1,25 +1,34 @@
 set -e
 
-# Get into a temp directory to run test from the installed scikit learn and
-# check if we do not leave artifacts
-mkdir -p $TEST_DIR
+run_tests() {
+    # Get into a temp directory to run test from the installed scikit learn and
+    # check if we  do not leave artifacts
+    mkdir -p $TEST_DIR
 
-cwd=`pwd`
-test_dir=$cwd/tests
-doctest_dir=$cwd/doc
+    cwd=`pwd`
+    test_dir=$cwd/tests
+    doctest_dir=$cwd/doc
 
-cd $TEST_DIR
+    cd $TEST_DIR
+    if [[ "$EXAMPLES" == "true" ]]; then
+        pytest -sv $test_dir/test_examples/
+    elif [[ "$DOCTEST" == "true" ]]; then
+        python -m doctest $doctest_dir/usage.rst
+    fi
 
-if [[ "$EXAMPLES" == "true" ]]; then
-    pytest -sv $test_dir/test_examples/
-elif [[ "$DOCTEST" == "true" ]]; then
-    python -m doctest $doctest_dir/usage.rst
+    if [[ "$COVERAGE" == "true" ]]; then
+        PYTEST_ARGS='--cov=openml'
+    else
+        PYTEST_ARGS=''
+    fi
+
+    pytest -n 4 --timeout=600 --timeout-method=thread -sv --ignore='test_OpenMLDemo.py' $PYTEST_ARGS $test_dir
+}
+
+if [[ "$RUN_FLAKE8" == "true" ]]; then
+    source ci_scripts/flake8_diff.sh
 fi
 
-if [[ "$COVERAGE" == "true" ]]; then
-    PYTEST_ARGS='--cov=openml'
-else
-    PYTEST_ARGS=''
+if [[ "$SKIP_TESTS" != "true" ]]; then
+    run_tests
 fi
-
-pytest -n 4 --timeout=600 --timeout-method=thread -sv --ignore='test_OpenMLDemo.py' $PYTEST_ARGS $test_dir
