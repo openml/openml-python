c7bb4c86bbc8d477433ea1c369479bbd55a8a619
bilgecelik <bilgecelik@gmail.com>
2018-09-20 16:17:59 +0200

    tasks fixes

--- openml/tasks/functions.py
+++ openml/tasks/functions.py
@@ -2,7 +2,6 @@ from collections import OrderedDict
 import io
 import re
 import os
-import shutil
 
 from oslo_concurrency import lockutils
 import xmltodict
@@ -10,20 +9,26 @@ import xmltodict
 from ..exceptions import OpenMLCacheException
 from ..datasets import get_dataset
 from .task import ClassificationTask, RegressionTask, ClusteringTask
-from .. import config
-from .._api_calls import _perform_api_call
-from ..utils import _create_cache_directory_for_id
 import openml.utils
+import openml._api_calls
+
+TASKS_CACHE_DIR_NAME = 'tasks'
 
 
 def _get_cached_tasks():
-    tasks = OrderedDict()
-    cache_dir = config.get_cache_directory()
+    """Return a dict of all the tasks which are cached locally.
+    Returns
+    -------
+    tasks : OrderedDict
+        A dict of all the cached tasks. Each task is an instance of
+        OpenMLTask.
+    """
 
-    task_cache_dir = os.path.join(cache_dir, "tasks")
+    tasks = OrderedDict()
+
+    task_cache_dir = openml.utils._create_cache_directory(TASKS_CACHE_DIR_NAME)
     directory_content = os.listdir(task_cache_dir)
     directory_content.sort()
-
     # Find all dataset ids for which we have downloaded the dataset
     # description
 
@@ -38,22 +43,31 @@ def _get_cached_tasks():
 
 
 def _get_cached_task(tid):
-    cache_dir = config.get_cache_directory()
-    task_cache_dir = os.path.join(cache_dir, "tasks")
-    task_file = os.path.join(task_cache_dir, str(tid), "task.xml")
+    """Return a cached task based on the given id.
+    Parameters
+    ----------
+    tid : int
+        Id of the task.
+    Returns
+    -------
+    OpenMLTask
+    """
+    tid_cache_dir = openml.utils._create_cache_directory_for_id(
+        TASKS_CACHE_DIR_NAME,
+        tid
+    )
 
     try:
-        with io.open(task_file, encoding='utf8') as fh:
-            task = _create_task_from_xml(xml=fh.read())
-        return task
+        with io.open(os.path.join(tid_cache_dir, "task.xml"), encoding='utf8') as fh:
+            return _create_task_from_xml(fh.read())
     except (OSError, IOError):
+        openml.utils._remove_cache_dir_for_id(TASKS_CACHE_DIR_NAME, tid_cache_dir)
         raise OpenMLCacheException("Task file for tid %d not "
                                    "cached" % tid)
 
 
 def _get_estimation_procedure_list():
     """Return a list of all estimation procedures which are on OpenML.
-
     Returns
     -------
     procedures : list
@@ -62,7 +76,7 @@ def _get_estimation_procedure_list():
         name, type, repeats, folds, stratified.
     """
 
-    xml_string = _perform_api_call("estimationprocedure/list")
+    xml_string = openml._api_calls._perform_api_call("estimationprocedure/list")
     procs_dict = xmltodict.parse(xml_string)
     # Minimalistic check if the XML is useful
     if 'oml:estimationprocedures' not in procs_dict:
@@ -80,12 +94,14 @@ def _get_estimation_procedure_list():
 
     procs = []
     for proc_ in procs_dict['oml:estimationprocedures']['oml:estimationprocedure']:
-        proc = {'id': int(proc_['oml:id']),
+        procs.append(
+            {
+                'id': int(proc_['oml:id']),
                 'task_type_id': int(proc_['oml:ttid']),
                 'name': proc_['oml:name'],
-                'type': proc_['oml:type']}
-
-        procs.append(proc)
+                'type': proc_['oml:type'],
+            }
+        )
 
     return procs
 
@@ -93,17 +109,14 @@ def _get_estimation_procedure_list():
 def list_tasks(task_type_id=None, offset=None, size=None, tag=None, **kwargs):
     """
     Return a number of tasks having the given tag and task_type_id
-
     Parameters
     ----------
     Filter task_type_id is separated from the other filters because
     it is used as task_type_id in the task description, but it is named
     type when used as a filter in list tasks call.
-
     task_type_id : int, optional
         ID of the task type as detailed
         `here <https://www.openml.org/search?type=task_type>`_.
-
         - Supervised classification: 1
         - Supervised regression: 2
         - Learning curve: 3
@@ -118,11 +131,9 @@ def list_tasks(task_type_id=None, offset=None, size=None, tag=None, **kwargs):
         the maximum number of tasks to show
     tag : str, optional
         the tag to include
-
     kwargs: dict, optional
         Legal filter operators: data_tag, status, data_id, data_name, number_instances, number_features,
         number_classes, number_missing_values.
-
     Returns
     -------
     dict
@@ -131,23 +142,20 @@ def list_tasks(task_type_id=None, offset=None, size=None, tag=None, **kwargs):
         task id, dataset id, task_type and status. If qualities are calculated
         for the associated dataset, some of these are also returned.
     """
-    return openml.utils.list_all(_list_tasks, task_type_id=task_type_id, offset=offset, size=size, tag=tag, **kwargs)
+    return openml.utils._list_all(_list_tasks, task_type_id=task_type_id, offset=offset, size=size, tag=tag, **kwargs)
 
 
 def _list_tasks(task_type_id=None, **kwargs):
     """
     Perform the api call to return a number of tasks having the given filters.
-
     Parameters
     ----------
     Filter task_type_id is separated from the other filters because
     it is used as task_type_id in the task description, but it is named
     type when used as a filter in list tasks call.
-
     task_type_id : int, optional
         ID of the task type as detailed
         `here <https://www.openml.org/search?type=task_type>`_.
-
         - Supervised classification: 1
         - Supervised regression: 2
         - Learning curve: 3
@@ -156,12 +164,10 @@ def _list_tasks(task_type_id=None, **kwargs):
         - Machine Learning Challenge: 6
         - Survival Analysis: 7
         - Subgroup Discovery: 8
-
     kwargs: dict, optional
         Legal filter operators: tag, data_tag, status, limit,
         offset, data_id, data_name, number_instances, number_features,
         number_classes, number_missing_values.
-
     Returns
     -------
     dict
@@ -177,27 +183,29 @@ def _list_tasks(task_type_id=None, **kwargs):
 
 def __list_tasks(api_call):
 
-    xml_string = _perform_api_call(api_call)
+    xml_string = openml._api_calls._perform_api_call(api_call)
     tasks_dict = xmltodict.parse(xml_string, force_list=('oml:task', 'oml:input'))
     # Minimalistic check if the XML is useful
     if 'oml:tasks' not in tasks_dict:
         raise ValueError('Error in return XML, does not contain "oml:runs": %s'
                          % str(tasks_dict))
     elif '@xmlns:oml' not in tasks_dict['oml:tasks']:
-        raise ValueError('Error in return XML, does not contain '	
+        raise ValueError('Error in return XML, does not contain '
                          '"oml:runs"/@xmlns:oml: %s'
                          % str(tasks_dict))
     elif tasks_dict['oml:tasks']['@xmlns:oml'] != 'http://openml.org/openml':
-        raise ValueError('Error in return XML, value of  '	
-                         '"oml:runs"/@xmlns:oml is not '	
+        raise ValueError('Error in return XML, value of  '
+                         '"oml:runs"/@xmlns:oml is not '
                          '"http://openml.org/openml": %s'
                          % str(tasks_dict))
+
     assert type(tasks_dict['oml:tasks']['oml:task']) == list, \
         type(tasks_dict['oml:tasks'])
 
     tasks = dict()
     procs = _get_estimation_procedure_list()
     proc_dict = dict((x['id'], x) for x in procs)
+
     for task_ in tasks_dict['oml:tasks']['oml:task']:
         tid = None
         try:
@@ -208,6 +216,7 @@ def __list_tasks(api_call):
                     'name': task_['oml:name'],
                     'task_type': task_['oml:task_type'],
                     'status': task_['oml:status']}
+
             # Other task inputs
             for input in task_.get('oml:input', list()):
                 if input['@name'] == 'estimation_procedure':
@@ -215,6 +224,7 @@ def __list_tasks(api_call):
                 else:
                     value = input.get('#text')
                     task[input['@name']] = value
+
             # The number of qualities can range from 0 to infinity
             for quality in task_.get('oml:quality', list()):
                 if '#text' not in quality:
@@ -235,19 +245,17 @@ def __list_tasks(api_call):
                 )
             else:
                 raise KeyError('Could not find key %s in %s!' % (e, task_))
+
     return tasks
 
 
 def get_tasks(task_ids):
     """Download tasks.
-
     This function iterates :meth:`openml.tasks.get_task`.
-
     Parameters
     ----------
     task_ids : iterable
         Integers representing task ids.
-
     Returns
     -------
     list
@@ -260,23 +268,19 @@ def get_tasks(task_ids):
 
 def get_task(task_id):
     """Download the OpenML task for a given task ID.
-
     Parameters
     ----------
     task_id : int
         The OpenML task id.
     """
-    try:
-        task_id = int(task_id)
-    except:
-        raise ValueError("Task ID is neither an Integer nor can be "
-                         "cast to an Integer.")
-
-    tid_cache_dir = _create_task_cache_dir(task_id)
+    task_id = int(task_id)
+    tid_cache_dir = openml.utils._create_cache_directory_for_id(
+        TASKS_CACHE_DIR_NAME, task_id,
+    )
 
     with lockutils.external_lock(
             name='task.functions.get_task:%d' % task_id,
-            lock_path=os.path.join(config.get_cache_directory(), 'locks'),
+            lock_path=openml.utils._create_lockfiles_dir(),
     ):
         try:
             task = _get_task_description(task_id)
@@ -284,9 +288,8 @@ def get_task(task_id):
             class_labels = dataset.retrieve_class_labels(task.target_name)
             task.class_labels = class_labels
             task.download_split()
-
         except Exception as e:
-            _remove_task_cache_dir(tid_cache_dir)
+            openml.utils._remove_cache_dir_for_id(TASKS_CACHE_DIR_NAME, tid_cache_dir)
             raise e
 
     return task
@@ -297,62 +300,15 @@ def _get_task_description(task_id):
     try:
         return _get_cached_task(task_id)
     except OpenMLCacheException:
-        xml_file = os.path.join(_create_task_cache_dir(task_id), "task.xml")
-        task_xml = _perform_api_call("task/%d" % task_id)
+        xml_file = os.path.join(
+            openml.utils._create_cache_directory_for_id(TASKS_CACHE_DIR_NAME, task_id),
+            "task.xml",
+        )
+        task_xml = openml._api_calls._perform_api_call("task/%d" % task_id)
 
         with io.open(xml_file, "w", encoding='utf8') as fh:
             fh.write(task_xml)
-        task = _create_task_from_xml(task_xml)
-
-    return task
-
-
-def _create_task_cache_directory(task_id):
-    """Create a task cache directory
-
-    In order to have a clearer cache structure and because every task
-    is cached in several files (description, split), there
-    is a directory for each task witch the task ID being the directory
-    name. This function creates this cache directory.
-
-    This function is NOT thread/multiprocessing safe.
-
-    Parameters
-    ----------
-    task_id : int
-        Task ID
-
-    Returns
-    -------
-    str
-        Path of the created dataset cache directory.
-    """
-    task_cache_dir = os.path.join(
-        config.get_cache_directory(), "tasks", str(task_id)
-    )
-    if os.path.exists(task_cache_dir) and os.path.isdir(task_cache_dir):
-        pass
-    elif os.path.exists(task_cache_dir) and not os.path.isdir(task_cache_dir):
-        raise ValueError('Task cache dir exists but is not a directory!')
-    else:
-        os.makedirs(task_cache_dir)
-    return task_cache_dir
-
-
-def _remove_task_cache_dir(tid_cache_dir):
-    """Remove the task cache directory
-
-    This function is NOT thread/multiprocessing safe.
-
-    Parameters
-    ----------
-    """
-    try:
-        shutil.rmtree(tid_cache_dir)
-    except (OSError, IOError):
-        raise ValueError('Cannot remove faulty task cache directory %s.'
-                         'Please do this manually!' % tid_cache_dir)
-
+        return _create_task_from_xml(task_xml)
 
 def _create_task_from_xml(xml):
     dic = xmltodict.parse(xml)["oml:task"]
