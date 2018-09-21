ae16ffb9809a1c594a663acff8c209d1c6dadf7f
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2018-09-21 09:20:17 +0200

    Remove print statements

--- tests/test_flows/test_flow.py
+++ tests/test_flows/test_flow.py
@@ -273,13 +273,11 @@ class TestFlow(TestBase):
         complicated = sklearn.pipeline.Pipeline(steps=steps)
 
         for classifier in [nb, complicated]:
-            print(classifier)
             flow = openml.flows.sklearn_to_flow(classifier)
             flow, _ = self._add_sentinel_to_flow_name(flow, None)
             #publish the flow
             flow = flow.publish()
             #redownload the flow
-            print(flow)
             flow = openml.flows.get_flow(flow.flow_id)
 
             # check if flow exists can find it
