1eafe18166e1dceb07c6a06058eaa1ee0f18216e
janvanrijn <janvanrijn@gmail.com>
2018-10-05 16:39:48 -0400

    initial commit

--- openml/setups/__init__.py
+++ openml/setups/__init__.py
@@ -1,4 +1,4 @@
-from .setup import OpenMLSetup
+from .setup import OpenMLSetup, OpenMLParameter
 from .functions import get_setup, list_setups, setup_exists, initialize_model
 
 __all__ = ['get_setup', 'list_setups', 'setup_exists', 'initialize_model']
---
+++ openml/setups/sklearn_converter.py
@@ -0,0 +1,22 @@
+from openml.flows import OpenMLFlow
+from openml.setups import OpenMLParameter
+
+
+def openml_param_name_to_sklearn(openml_parameter, flow):
+    """
+    Converts the name of an OpenMLParameter into the sklean name, given a flow.
+    Note that the same parameter might have a different name in various flows
+    (e.g., the parameter )
+
+    Parameters
+    ----------
+    openml_parameter: OpenMLParameter
+        The parameter under consideration
+
+    flow: OpenMLFlow
+        The flow that provides context.
+    """
+    if not isinstance(openml_parameter, OpenMLParameter):
+        raise ValueError('openml_parameter should be an instance of OpenMLParameter')
+    if not isinstance(flow, OpenMLFlow):
+        raise ValueError('flow should be an instance of OpenMLFlow')
