a32221739910e01e9820bf5c881eb1cd1161ab46
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2019-02-18 22:08:43 +0100

    simplify unit test

--- tests/test_runs/test_run_functions.py
+++ tests/test_runs/test_run_functions.py
@@ -101,12 +101,10 @@ class TestRun(TestBase):
             for col_idx in compare_slice:
                 val_1 = predictions['data'][idx][col_idx]
                 val_2 = predictions_prime['data'][idx][col_idx]
-                if type(val_1) == type(val_2):
-                    self.assertEqual(val_1, val_2)
-                elif type(val_1) == float or type(val_2) == float:
-                    self.assertTrue(abs(float(val_1) - float(val_2)) < 0.00001)
+                if type(val_1) == float or type(val_2) == float:
+                    self.assertAlmostEqual(float(val_1), float(val_2))
                 else:
-                    self.assertEqual(str(val_1), str(val_2))
+                    self.assertEqual(val_1, val_2)
 
         return True
 
