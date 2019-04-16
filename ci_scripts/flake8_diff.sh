973d48a3c8f211e10bfcf12f5a5ae8d6e06d860b
PGijsbers <p.gijsbers@tue.nl>
2019-04-16 16:24:40 +0200

    Add note to update CONTRIBUTING.md if things in this script change.

--- ci_scripts/flake8_diff.sh
+++ ci_scripts/flake8_diff.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Update /CONTRIBUTING.md if these commands change.
+# The reason for not advocating using this script directly is that it
+# might not work out of the box on Windows.
 flake8 --ignore E402,W503 --show-source --max-line-length 100 $options
 mypy openml --ignore-missing-imports --follow-imports skip
