84b38fa1d4770855a8800c478348aaf0303246cb
PGijsbers <p.gijsbers@tue.nl>
2019-04-17 10:41:06 +0200

    Overwrite default code highlighting styles in favor of something with more contrast.

--- doc/conf.py
+++ doc/conf.py
@@ -214,7 +214,7 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["_static"]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -358,3 +358,7 @@ sphinx_gallery_conf = {
     'filename_pattern': '.*example.py$|.*tutorial.py$',
     # TODO: fix back/forward references for the examples.
 }
+
+
+def setup(app):
+    app.add_stylesheet("codehighlightstyle.css")
