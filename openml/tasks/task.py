3e186e858559c537dfc2b47f153dd11f3768ca1e
ArlindKadra <arlindkadra@gmail.com>
2019-04-18 10:49:45 +0100

    Fixing bug with unit tests of clustering tasks, changing order for type annotations

--- openml/tasks/task.py
+++ openml/tasks/task.py
@@ -129,7 +129,7 @@ class OpenMLTask(ABC):
         task_container = OrderedDict()  # type: OrderedDict[str, OrderedDict]
         task_dict = OrderedDict([
             ('@xmlns:oml', 'http://openml.org/openml')
-        ])  # type: OrderedDict[str, Union[Union[int, str], List]]
+        ])  # type: OrderedDict[str, Union[Union[List, str], int]]
 
         task_container['oml:task_inputs'] = task_dict
         task_dict['oml:task_type_id'] = self.task_type_id
--- tests/test_tasks/test_clustering_task.py
+++ tests/test_tasks/test_clustering_task.py
@@ -11,14 +11,17 @@ class OpenMLClusteringTaskTest(OpenMLTaskTest):
         self.task_id = 146714
         self.task_type_id = 5
         self.estimation_procedure = 17
+        self.production_server = "https://openml.org/api/v1/xml"
 
     def test_get_dataset(self):
-
-        task = openml.tasks.get_task(self.task_id)
+        # no clustering tasks on test server
+        openml.config.server = self.production_server
+        task = super(OpenMLClusteringTaskTest, self).test_download_task()
         task.get_dataset()
 
     def test_download_task(self):
-
+        # no clustering tasks on test server
+        openml.config.server = self.production_server
         task = super(OpenMLClusteringTaskTest, self).test_download_task()
         self.assertEqual(task.task_id, self.task_id)
         self.assertEqual(task.task_type_id, 5)
