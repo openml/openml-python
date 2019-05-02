1ee583b9a114c080ba3b27211847e0100d88cbc8
neeratyoy <neeratyoy@gmail.com>
2019-05-02 18:47:59 +0200

    Adding boxplot to compare flows

--- examples/fetch_evaluations.py
+++ examples/fetch_evaluations.py
@@ -11,8 +11,8 @@ from pprint import pprint
 
 ############################################################################
 #
-# Evalutions contain details of all runs and the resulting results that
-# was uploaded for those settings - data, flow, task, etc.
+# Evalutions contain details (IDs and names) of data, flow, tasks, of all runs
+# and the resulting results that was uploaded for those settings.
 # The listing functions take optional parameters which can be used to filter
 # results and fetch only the evaluations required.
 #
@@ -23,10 +23,7 @@ from pprint import pprint
 # Listing evaluations
 # ^^^^^^^^^^^^^^^^^^^
 #
-# We shall retrieve a small list and test the listing function for evaluations
-evals = openml.evaluations.list_evaluations(function='predictive_accuracy', size=10)
-pprint(evals)
-# To have a tabular output
+# We shall retrieve a small set to test the listing function for evaluations
 openml.evaluations.list_evaluations(function='predictive_accuracy', size=10,
                                     output_format='dataframe')
 # Using other evaluation metrics
@@ -38,13 +35,14 @@ openml.evaluations.list_evaluations(function='precision', size=10,
 #
 # We will start by displaying a simple *supervised classification* task:
 task_id = 167140        # https://www.openml.org/t/167140
-task = openml.tasks.get_tasks([task_id])[0]
+task = openml.tasks.get_task(task_id)
 pprint(vars(task))
 
 # Obtaining all the evaluations for the task
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-evals = openml.evaluations.list_evaluations(function='predictive_accuracy', task=[task_id],
+metric = 'predictive_accuracy'
+evals = openml.evaluations.list_evaluations(function=metric, task=[task_id],
                                             output_format='dataframe')
 # Displaying the first 10 rows
 pprint(evals.head(n=10))
@@ -52,14 +50,14 @@ pprint(evals.head(n=10))
 evals = evals.sort_values(by='value', ascending=False)
 pprint(evals.head())
 
-# Obtain CDF
-# ^^^^^^^^^^
+# Obtain CDF of metric for chosen task
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
 from matplotlib import pyplot as plt
 
 
 def plot_cdf(values, metric='predictive_accuracy'):
-    plt.hist(values, density=True, histtype='step', cumulative=True)
+    plt.hist(values, density=True, histtype='step', cumulative=True, linewidth=3)
     plt.xlim(max(0, min(values) - 0.1), 1)
     plt.title('CDF')
     plt.xlabel(metric)
@@ -69,4 +67,35 @@ def plot_cdf(values, metric='predictive_accuracy'):
     plt.show()
 
 
-plot_cdf(evals.value)
+plot_cdf(evals.value, metric)
+
+# Compare top 10 performing flows
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+import numpy as np
+import pandas as pd
+
+
+def plot_flow_compare(evaluations, top_n=10, metric='predictive_accuracy'):
+    # Collecting the top 10 performing unique flow_id
+    flow_list = np.unique(evaluations.flow_id)[:10]
+
+    df = pd.DataFrame()
+    for i in range(len(flow_list)):
+        df = pd.concat([df, pd.DataFrame(evaluations[evaluations.flow_id == flow_list[i]].value)],
+                       ignore_index=True, axis=1)
+    fig, axs = plt.subplots()
+    df.boxplot()
+    axs.set_title('Boxplot comparing ' + metric + ' for different flows')
+    axs.set_ylabel(metric)
+    axs.set_xlabel('Flow ID')
+    axs.set_xticklabels(flow_list)
+    flow_freq = list(df.count(axis=0, numeric_only=True))
+    print(len(flow_freq), flow_freq)
+    print(len(flow_list), flow_list)
+    for i in range(len(flow_list)):
+        axs.text(i + 1.05, np.nanmin(df.values), str(flow_freq[i]) + ' run(s)')
+    plt.show()
+
+
+plot_flow_compare(evals, metric=metric)
