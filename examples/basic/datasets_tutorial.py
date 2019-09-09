86ee9b201d6ac9be17e372dd4b1f5126e97efb9f
ArlindKadra <arlindkadra@gmail.com>
2019-09-09 17:53:16 +0200

    First commit of basic examples

---
+++ examples/basic/datasets_tutorial.py
@@ -0,0 +1,27 @@
+"""
+========
+Datasets
+========
+
+How to list and download datasets.
+"""
+############################################################################
+import openml
+
+############################################################################
+# List datasets
+
+datasets_df = openml.datasets.list_datasets(output_format='dataframe')
+datasets_df.head(n=10)
+
+############################################################################
+# Download datasets
+
+first_dataset_id = int(datasets_df['did'].iloc[0])
+dataset = openml.datasets.get_dataset(first_dataset_id)
+
+# Print a summary
+print("This is dataset '%s', the target feature is '%s'" %
+      (dataset.name, dataset.default_target_attribute))
+print("URL: %s" % dataset.url)
+print(dataset.description[:500])
---
+++ examples/basic/tasks_tutorial.py
@@ -0,0 +1,21 @@
+"""
+Tasks
+=====
+
+A tutorial on how to list and download tasks.
+"""
+import openml
+
+############################################################################
+# Listing tasks
+
+# Listing only supervised classification tasks (task type 1) from all `task types <https://www.openml.org/search?type=task_type>`_.
+tasks_df = openml.tasks.list_tasks(task_type_id=1, output_format='dataframe')
+tasks_df.head()
+
+############################################################################
+# Downloading tasks
+
+first_task_id = int(tasks_df['tid'].iloc[0])
+task = openml.tasks.get_task(tasks_df)
+print(task)
