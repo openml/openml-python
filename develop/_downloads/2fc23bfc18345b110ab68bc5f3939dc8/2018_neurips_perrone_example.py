5a44c08a81143b0171c3ad55ceaf4b095a1ecfa4
Arlind Kadra <arlindkadra@gmail.com>
2019-09-30 12:20:58 +0000

    Deploy openml/openml-python to github.com/openml/openml-python.git:gh-pages

---
+++ develop/_downloads/2fc23bfc18345b110ab68bc5f3939dc8/2018_neurips_perrone_example.py
@@ -0,0 +1,17 @@
+"""
+Perrone et al. (2018)
+=====================
+
+A tutorial on how to build a surrogate model based on OpenML data as done for *Scalable
+Hyperparameter Transfer Learning* by Perrone et al..
+
+Publication
+~~~~~~~~~~~
+
+| Scalable Hyperparameter Transfer Learning
+| Valerio Perrone and Rodolphe Jenatton and Matthias Seeger and Cedric Archambeau
+| In *Advances in Neural Information Processing Systems 31*, 2018
+| Available at http://papers.nips.cc/paper/7917-scalable-hyperparameter-transfer-learning.pdf
+
+This is currently a placeholder.
+"""
