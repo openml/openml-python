4063646689789a664e240b4521ec8cae0ebbce1f
janvanrijn <janvanrijn@gmail.com>
2019-10-01 23:22:42 +0200

    typo fix

--- examples/40_paper/2018_ida_strang_example.py
+++ examples/40_paper/2018_ida_strang_example.py
@@ -9,7 +9,7 @@ Publication
 ~~~~~~~~~~~
 
 | Don't Rule Out Simple Models Prematurely: A Large Scale Benchmark Comparing Linear and Non-linear Classifiers in OpenML
-| Benjamin Strang, Pieter Putten, Jan van Rijn and Frank Hutter
+| Benjamin Strang, Peter van der Putten, Jan N. van Rijn and Frank Hutter
 | In *Advances in Intelligent Data Analysis XVII 17th International Symposium*, 2018
 | Available at https://link.springer.com/chapter/10.1007%2F978-3-030-01768-2_25
 """
