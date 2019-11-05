4daf639d3e07f6427c620d8566645144a29013d9
Arlind Kadra <arlindkadra@gmail.com>
2019-11-05 18:10:42 +0000

    Deploy openml/openml-python to github.com/openml/openml-python.git:gh-pages

---
+++ master/_downloads/006bd0fc770c8918e54252890f1e023e/study_tutorial.py
@@ -0,0 +1,112 @@
+"""
+=================
+Benchmark studies
+=================
+
+How to list, download and upload benchmark studies.
+
+In contrast to `benchmark suites <https://docs.openml.org/benchmark/#benchmarking-suites>`_ which
+hold a list of tasks, studies hold a list of runs. As runs contain all information on flows and
+tasks, all required information about a study can be retrieved.
+"""
+############################################################################
+import uuid
+
+import numpy as np
+import sklearn.tree
+import sklearn.pipeline
+import sklearn.impute
+
+import openml
+
+
+############################################################################
+# .. warning:: This example uploads data. For that reason, this example
+#   connects to the test server at test.openml.org before doing so.
+#   This prevents the crowding of the main server with example datasets,
+#   tasks, runs, and so on.
+############################################################################
+
+
+############################################################################
+# Listing studies
+# ***************
+#
+# * Use the output_format parameter to select output type
+# * Default gives ``dict``, but we'll use ``dataframe`` to obtain an
+#   easier-to-work-with data structure
+
+studies = openml.study.list_studies(output_format='dataframe', status='all')
+print(studies.head(n=10))
+
+
+############################################################################
+# Downloading studies
+# ===================
+
+############################################################################
+# This is done based on the study ID.
+study = openml.study.get_study(123)
+print(study)
+
+############################################################################
+# Studies also features a description:
+print(study.description)
+
+############################################################################
+# Studies are a container for runs:
+print(study.runs)
+
+############################################################################
+# And we can use the evaluation listing functionality to learn more about
+# the evaluations available for the conducted runs:
+evaluations = openml.evaluations.list_evaluations(
+    function='predictive_accuracy',
+    output_format='dataframe',
+    study=study.study_id,
+)
+print(evaluations.head())
+
+############################################################################
+# Uploading studies
+# =================
+#
+# Creating a study is as simple as creating any kind of other OpenML entity.
+# In this examples we'll create a few runs for the OpenML-100 benchmark
+# suite which is available on the OpenML test server.
+
+openml.config.start_using_configuration_for_example()
+
+# Very simple classifier which ignores the feature type
+clf = sklearn.pipeline.Pipeline(steps=[
+    ('imputer', sklearn.impute.SimpleImputer()),
+    ('estimator', sklearn.tree.DecisionTreeClassifier(max_depth=5)),
+])
+
+suite = openml.study.get_suite(1)
+# We'll create a study with one run on three random datasets each
+tasks = np.random.choice(suite.tasks, size=3, replace=False)
+run_ids = []
+for task_id in tasks:
+    task = openml.tasks.get_task(task_id)
+    run = openml.runs.run_model_on_task(clf, task)
+    run.publish()
+    run_ids.append(run.run_id)
+
+# The study needs a machine-readable and unique alias. To obtain this,
+# we simply generate a random uuid.
+alias = uuid.uuid4().hex
+
+new_study = openml.study.create_study(
+    name='Test-Study',
+    description='Test study for the Python tutorial on studies',
+    run_ids=run_ids,
+    alias=alias,
+    benchmark_suite=suite.study_id,
+)
+new_study.publish()
+print(new_study)
+
+
+############################################################################
+openml.config.stop_using_configuration_for_example()
---
+++ master/_downloads/cb73f5fc41065b327cef9d079472032b/simple_flows_and_runs_tutorial.py
@@ -0,0 +1,48 @@
+"""
+Flows and Runs
+==============
+
+A simple tutorial on how to train/run a model and how to upload the results.
+"""
+
+import openml
+from sklearn import ensemble, neighbors
+
+############################################################################
+# Train a machine learning model
+# ==============================
+#
+# .. warning:: This example uploads data. For that reason, this example
+#   connects to the test server at test.openml.org. This prevents the main
+#   server from crowding with example datasets, tasks, runs, and so on.
+
+openml.config.start_using_configuration_for_example()
+
+# NOTE: We are using dataset 20 from the test server: https://test.openml.org/d/20
+dataset = openml.datasets.get_dataset(20)
+X, y, categorical_indicator, attribute_names = dataset.get_data(
+    dataset_format='array',
+    target=dataset.default_target_attribute
+)
+clf = neighbors.KNeighborsClassifier(n_neighbors=3)
+clf.fit(X, y)
+
+############################################################################
+# Running a model on a task
+# =========================
+
+task = openml.tasks.get_task(119)
+clf = ensemble.RandomForestClassifier()
+run = openml.runs.run_model_on_task(clf, task)
+print(run)
+
+############################################################################
+# Publishing the run
+# ==================
+
+myrun = run.publish()
+print("Run was uploaded to http://test.openml.org/r/" + str(myrun.run_id))
+print("The flow can be found at http://test.openml.org/f/" + str(myrun.flow_id))
+
+############################################################################
+openml.config.stop_using_configuration_for_example()
