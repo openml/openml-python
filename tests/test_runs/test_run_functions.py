85b7b53c2fd5ef0ecf833dd95ad7ad4cfa6da71a
sahithyaravi1493 <sahithyaravi1493@gmail.com>
2019-11-06 10:34:47 +0100

    length of run change

--- tests/test_runs/test_run_functions.py
+++ tests/test_runs/test_run_functions.py
@@ -1126,7 +1126,7 @@ class TestRun(TestBase):
         # error_message and run_details exist, too, but are not used so far. We need to update
         # this check once they are used!
         self.assertIsInstance(run, dict)
-        assert len(run) == 7, str(run)
+        assert len(run) == 8, str(run)
 
     def test_get_runs_list(self):
         # TODO: comes from live, no such lists on test
