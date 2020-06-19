7b396426efd2280d6ca7a2776bedefa8fd249f36
neeratyoy <neeratyoy@gmail.com>
2020-06-19 14:30:58 +0200

    Adding Python 3.8 support

--- setup.py
+++ setup.py
@@ -93,4 +93,5 @@ setuptools.setup(name="openml",
                               'Operating System :: MacOS',
                               'Programming Language :: Python :: 3',
                               'Programming Language :: Python :: 3.6',
-                              'Programming Language :: Python :: 3.7'])
+                              'Programming Language :: Python :: 3.7',
+                              'Programming Language :: Python :: 3.8'])
