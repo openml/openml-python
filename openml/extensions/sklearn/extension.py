5d9c69c210792d8b447c8b17d466ac44e41d0eb2
zikun <33176974+zikun@users.noreply.github.com>
2020-08-03 22:48:44 +0800

    Add flake8-print in pre-commit (#939)
    * Add flake8-print in pre-commit config
    
    * Replace print statements with logging

--- openml/extensions/sklearn/extension.py
+++ openml/extensions/sklearn/extension.py
@@ -1316,7 +1316,7 @@ class SklearnExtension(Extension):
                         "Could not find attribute "
                         "param_distributions."
                     )
-                print(
+                logger.warning(
                     "Warning! Using subclass BaseSearchCV other than "
                     "{GridSearchCV, RandomizedSearchCV}. "
                     "Should implement param check. "
