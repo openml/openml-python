87b1d49ba7ad5acdda7a2577b982996c3efb35e7
Arlind Kadra <arlindkadra@gmail.com>
2020-10-29 13:43:53 +0100

    Removing comment as it serves no purpose

--- tests/test_datasets/test_dataset.py
+++ tests/test_datasets/test_dataset.py
@@ -220,7 +220,6 @@ class OpenMLDatasetTest(TestBase):
         self.assertEqual(xy.shape, (150, 5))
 
     def test_equality_comparison(self):
-        # verify the implemented comparison of the dataset object
         self.assertTrue(self.iris == self.iris)
         self.assertFalse(self.iris == self.titanic)
         self.assertFalse(self.titanic == 'Wrong_object')
