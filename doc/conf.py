7b991b223db3ea62f56e23f08578e373a339ebc9
PGijsbers <p.gijsbers@tue.nl>
2020-11-16 14:02:26 +0100

    Refactor copyright message generation
    u-strings are not needed in py3, directly getting the year from localtime is more readable

--- doc/conf.py
+++ doc/conf.py
@@ -65,9 +65,7 @@ master_doc = "index"
 
 # General information about the project.
 project = u"OpenML"
-copyright = u"2014-{}, the OpenML-Python team".format(
-    time.strftime("%Y,%m,%d,%H,%M,%S").split(",")[0]
-)
+copyright = f"2014-{time.localtime().tm_year}, the OpenML-Python team"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
