436a9fe01334565660c3a1a0de3e462bcf9da203
neeratyoy <neeratyoy@gmail.com>
2020-12-09 15:19:58 +0100

    Handling task exists check

--- tests/test_tasks/test_regression_task.py
+++ tests/test_tasks/test_regression_task.py
@@ -1,11 +1,13 @@
 # License: BSD 3-Clause
 
+import ast
 import numpy as np
 
 import openml
 from openml.tasks import TaskType
 from openml.testing import TestBase
 from openml.utils import check_task_existence
+from openml.exceptions import OpenMLServerException
 from .test_supervised_task import OpenMLSupervisedTaskTest
 
 
@@ -29,8 +31,16 @@ class OpenMLRegressionTaskTest(OpenMLSupervisedTaskTest):
             task_meta_data["task_type"] = TaskType.SUPERVISED_REGRESSION
             new_task = openml.tasks.create_task(**task_meta_data)
             # publishes the new task
-            new_task = new_task.publish()
-            task_id = new_task.task_id
+            try:
+                new_task = new_task.publish()
+                task_id = new_task.task_id
+            except OpenMLServerException as e:
+                if e.code == 614:  # Task already exists
+                    # the exception message contains the task_id that was matched in the format
+                    # 'Task already exists. - matched id(s): [xxxx]'
+                    task_id = ast.literal_eval(e.message.split("matched id(s):")[-1].strip())[0]
+                else:
+                    raise Exception(repr(e))
             # mark to remove the uploaded task
             TestBase._mark_entity_for_removal("task", task_id)
             TestBase.logger.info("collected from test_run_functions: {}".format(task_id))
