ba344156d39cbe0bed90721e6ce36cd61349262b
neeratyoy <neeratyoy@gmail.com>
2021-05-04 14:29:46 +0200

    Adding new example

---
+++ examples/30_extended/blackbox_optimization_tutorial.py
@@ -0,0 +1,17 @@
+"""
+Blackbox Optimization
+=====================
+"""
+
+# License: BSD 3-Clause
+
+import openml
+
+
+############################################################################
+# We'll use the test server for the rest of this tutorial.
+#
+# .. warning::
+#    .. include:: ../../test_server_usage_warning.txt
+openml.config.start_using_configuration_for_example()
+
