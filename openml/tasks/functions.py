926f14682b448debbab058117c801e835fa65b4c
PGijsbers <p.gijsbers@tue.nl>
2023-03-03 16:59:36 +0100

    Document unwrapped error codes and add explicit 455 error

--- openml/tasks/functions.py
+++ openml/tasks/functions.py
@@ -567,6 +567,8 @@ def delete_task(task_id: int) -> bool:
         return openml.utils._delete_entity("task", task_id)
     except OpenMLServerException as e:
         # https://github.com/openml/OpenML/blob/21f6188d08ac24fcd2df06ab94cf421c946971b0/openml_OS/views/pages/api_new/v1/xml/pre.php#L234-L239  # noqa: 501
+        # 451 isn't actually used but 103 is returned instead
+        # 452 is descriptive enough: 'Task does not exist - None'
         if e.code == 453:
             raise OpenMLNotAuthorizedError(
                 message="The task can not be deleted because it was not uploaded by you.",
@@ -576,6 +578,15 @@ def delete_task(task_id: int) -> bool:
         if e.code == 454:
             raise OpenMLNotAuthorizedError(
                 message="The task can not be deleted because it still has associated runs.",
+                code=e.code,
+                url=e.url,
+            )
+        if e.code == 455:
+            raise OpenMLServerException(
+                message=(
+                    "The task can not be deleted for unknown reason, please open an issue at: "
+                    "https://github.com/openml/openml-python/issues/new"
+                ),
                 code=e.code,
                 url=e.url,
             )
