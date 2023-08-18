3c29b1d5ef06b136099680b7c4800857fa4d6e4f
Vishal Parmar <vishalm524112@gmail.com>
2023-08-18 16:42:33 +0530

    Update openml/runs/trace.py
    Co-authored-by: Lennart Purucker <contact@lennart-purucker.com>

--- openml/runs/trace.py
+++ openml/runs/trace.py
@@ -34,8 +34,7 @@ class OpenMLRunTrace(object):
     """
 
     def __init__(self, run_id: int, trace_iterations: List[List]):
-        """
-        Object to hold the trace content of a run.
+        """Object to hold the trace content of a run.
         
         Parameters
         ----------
