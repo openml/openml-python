e5267d68e7d9a591b63ec8cb97cf275ffacf85e7
taniya-das <taniyatd30@gmail.com>
2025-06-18 17:08:09 +0200

    convert test_get_sparse_daatset_rowid_and_ignore_and_target

--- tests/test_datasets/test_dataset.py
+++ tests/test_datasets/test_dataset.py
@@ -348,34 +348,6 @@ class OpenMLDatasetTestSparse(TestBase):
 
         self.sparse_dataset = openml.datasets.get_dataset(4136, download_data=False)
 
-
-    def test_get_sparse_dataset_dataframe(self):
-        rval, *_ = self.sparse_dataset.get_data()
-        assert isinstance(rval, pd.DataFrame)
-        np.testing.assert_array_equal(
-            [pd.SparseDtype(np.float32, fill_value=0.0)] * len(rval.dtypes),
-            rval.dtypes,
-        )
-        assert rval.shape == (600, 20001)
-
-    def test_get_sparse_dataset_rowid_and_ignore_and_target(self):
-        # TODO: re-add row_id and ignore attributes
-        self.sparse_dataset.ignore_attribute = ["V256"]
-        self.sparse_dataset.row_id_attribute = ["V512"]
-        X, y, categorical, _ = self.sparse_dataset.get_data(
-            target="class",
-            include_row_id=False,
-            include_ignore_attribute=False,
-        )
-        assert all(dtype == pd.SparseDtype(np.float32, fill_value=0.0) for dtype in X.dtypes)
-        # array format returned dense, but now we only return sparse and let the user handle it.
-        assert isinstance(y.dtypes, pd.SparseDtype)
-        assert X.shape == (600, 19998)
-
-        assert len(categorical) == 19998
-        self.assertListEqual(categorical, [False] * 19998)
-        assert y.shape == (600,)
-
     def test_get_sparse_categorical_data_id_395(self):
         dataset = openml.datasets.get_dataset(395, download_data=True)
         # breakpoint()
@@ -387,8 +359,58 @@ class OpenMLDatasetTestSparse(TestBase):
         assert feature.data_type == "nominal"
         assert len(feature.nominal_values) == 25
 
+@pytest.mark.production
+def test_get_sparse_dataset_rowid_and_ignore_and_target(requests_mock, test_files_directory):
+    content_file = (
+        test_files_directory / "mock_responses" / "datasets" / "sparse_dataset" /"data_description.xml"
+    )
+    requests_mock.get("https://www.openml.org/api/v1/xml/data/4136", text=content_file.read_text())
+    sparse_dataset = openml.datasets.get_dataset(4136, download_data=False)
+    
+    # TODO: re-add row_id and ignore attributes
+    sparse_dataset.ignore_attribute = ["V2"]
+    sparse_dataset.row_id_attribute = ["V5"]
+    
+    sparse_arff_file = (
+        test_files_directory / "mock_responses" / "datasets" / "sparse_dataset" /"sparse_arff.arff"
+    )
+    requests_mock.get("https://api.openml.org/data/v1/download/1681111/Dexter.sparse_arff", text = sparse_arff_file.read_text())
+    
+    X, y, categorical, _ = sparse_dataset.get_data(
+        target="class",
+        include_row_id=False,
+        include_ignore_attribute=False,
+    )
+    assert all(dtype == pd.SparseDtype(np.float32, fill_value=0.0) for dtype in X.dtypes)
+    # array format returned dense, but now we only return sparse and let the user handle it.
+    assert isinstance(y.dtypes, pd.SparseDtype)
+    assert X.shape == (10, 8)
 
+    assert len(categorical) == 8
+    assert categorical ==  [False] * 8
+    assert y.shape == (10,)
 
+@pytest.mark.production
+def test_get_sparse_dataset_dataframe(requests_mock, test_files_directory):
+    content_file = (
+        test_files_directory / "mock_responses" / "datasets" / "sparse_dataset" /"data_description.xml"
+    )
+    requests_mock.get("https://www.openml.org/api/v1/xml/data/4136", text=content_file.read_text())
+    sparse_dataset = openml.datasets.get_dataset(4136, download_data=False)
+    
+    sparse_arff_file = (
+        test_files_directory / "mock_responses" / "datasets" / "sparse_dataset" /"sparse_arff.arff"
+    )
+    requests_mock.get("https://api.openml.org/data/v1/download/1681111/Dexter.sparse_arff", text = sparse_arff_file.read_text())
+    rval, *_ = sparse_dataset.get_data()
+    
+    assert isinstance(rval, pd.DataFrame)
+    np.testing.assert_array_equal(
+        [pd.SparseDtype(np.float32, fill_value=0.0)] * len(rval.dtypes),
+        rval.dtypes,
+    )
+    assert rval.shape == (10, 11)
+    
 @pytest.mark.production
 def test_get_sparse_dataset_dataframe_with_target(requests_mock, test_files_directory):
     
