a4ff70bb3903d3e0dd58357ca1545ef6d314a465
PGijsbers <p.gijsbers@tue.nl>
2025-06-20 11:06:20 +0200

    Fix broken test

--- tests/test_openml/test_config.py
+++ tests/test_openml/test_config.py
@@ -175,13 +175,14 @@ def test_configuration_file_not_overwritten_on_load():
 
 def test_configuration_loads_booleans(tmp_path):
     config_file_content = "avoid_duplicate_runs=true\nshow_progress=false"
-    with (tmp_path / "config").open("w") as config_file:
+    tmp_file = tmp_path / "config"
+    with tmp_file.open("w") as config_file:
         config_file.write(config_file_content)
-        read_config = openml.config._parse_config(tmp_path)
+    read_config = openml.config._parse_config(tmp_file)
 
     # Explicit test to avoid truthy/falsy modes of other types
-    assert True == read_config["avoid_duplicate_runs"]
-    assert False == read_config["show_progress"]
+    assert read_config["avoid_duplicate_runs"] is True
+    assert read_config["show_progress"] is False
 
 
 def test_openml_cache_dir_env_var(tmp_path: Path) -> None:
