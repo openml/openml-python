f61b5eb8fcfa12927fb0abca396117b74ccff858
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2025-11-15 03:15:15 +0530

    Cleanup
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,7 +33,7 @@ import os
 import shutil
 from pathlib import Path
 import pytest
-# import openml_sklearn
+import openml_sklearn
 
 import openml
 from openml.testing import TestBase
--- tests/test_study/test_study_functions.py
+++ tests/test_study/test_study_functions.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 import pytest
-# import unittest
 
 import openml
 import openml.study
