e92156aa4b4d94bf41ed009a2261e1c25864cb70
Jigyasu <jigyasu@outlook.in>
2026-01-05 16:36:55 +0530

    move the flow utils to flows/functions.py

--- openml/flows/functions.py
+++ openml/flows/functions.py
@@ -14,6 +14,7 @@ import xmltodict
 import openml._api_calls
 import openml.utils
 from openml.exceptions import OpenMLCacheException
+from openml.extensions.registry import resolve_api_connector
 
 from . import OpenMLFlow
 
@@ -552,3 +553,37 @@ def delete_flow(flow_id: int) -> bool:
         True if the deletion was successful. False otherwise.
     """
     return openml.utils._delete_entity("flow", flow_id)
+
+
+def flow_to_estimator(flow: OpenMLFlow) -> Any:
+    """Convert an OpenML flow to an estimator instance.
+
+    Parameters
+    ----------
+    flow : openml.flows.OpenMLFlow
+        The OpenML flow to convert.
+
+    Returns
+    -------
+    estimator_instance : Any
+        The corresponding estimator instance.
+    """
+    connector = resolve_api_connector(flow)
+    return connector.serializer().flow_to_model(flow)
+
+
+def estimator_to_flow(estimator_instance: Any) -> OpenMLFlow:
+    """Convert an estimator instance to an OpenML flow.
+
+    Parameters
+    ----------
+    estimator_instance : Any
+        The estimator instance to convert.
+
+    Returns
+    -------
+    flow : openml.flows.OpenMLFlow
+        The corresponding OpenML flow.
+    """
+    connector = resolve_api_connector(estimator_instance)
+    return connector.serializer().model_to_flow(estimator_instance)
