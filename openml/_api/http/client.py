5dfcbce55a027d19cd502ea7bb3d521c2b1bca29
geetu040 <raoarmaghanshakir040@gmail.com>
2026-01-07 22:14:31 +0500

    refactor

--- openml/_api/http/client.py
+++ openml/_api/http/client.py
@@ -1,24 +1,30 @@
 from __future__ import annotations
 
-from typing import Any, Mapping
+from typing import TYPE_CHECKING, Any, Mapping
 
 import requests
 from requests import Response
 
 from openml.__version__ import __version__
 
+if TYPE_CHECKING:
+    from openml._api.config import APIConfig
+
 
 class HTTPClient:
-    def __init__(self, base_url: str) -> None:
-        self.base_url = base_url
+    def __init__(self, config: APIConfig) -> None:
+        self.config = config
         self.headers: dict[str, str] = {"user-agent": f"openml-python/{__version__}"}
+
+    def _create_url(self, path: str) -> str:
+        return self.config.server + self.config.base_url + path
 
     def get(
         self,
         path: str,
         params: Mapping[str, Any] | None = None,
     ) -> Response:
-        url = f"{self.base_url}/{path}"
+        url = self._create_url(path)
         return requests.get(url, params=params, headers=self.headers, timeout=10)
 
     def post(
@@ -27,7 +33,7 @@ class HTTPClient:
         data: Mapping[str, Any] | None = None,
         files: Any = None,
     ) -> Response:
-        url = f"{self.base_url}/{path}"
+        url = self._create_url(path)
         return requests.post(url, data=data, files=files, headers=self.headers, timeout=10)
 
     def delete(
@@ -35,5 +41,5 @@ class HTTPClient:
         path: str,
         params: Mapping[str, Any] | None = None,
     ) -> Response:
-        url = f"{self.base_url}/{path}"
+        url = self._create_url(path)
         return requests.delete(url, params=params, headers=self.headers, timeout=10)
