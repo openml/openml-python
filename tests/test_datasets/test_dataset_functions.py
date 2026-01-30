0b989d151e45899c0cba0f7981938b293668ad82
PGijsbers <p.gijsbers@tue.nl>
2026-01-30 11:27:52 +0100

    Fix typo

--- tests/test_datasets/test_dataset_functions.py
+++ tests/test_datasets/test_dataset_functions.py
@@ -879,7 +879,7 @@ class TestOpenMLDataset(TestBase):
 
     @pytest.mark.uses_test_server()
     def test_get_online_dataset_arff(self):
-        dataset_id = 128  # iris -- one of the few datasets with parquet file
+        dataset_id = 128  # iris -- one of the few datasets without parquet file
         # lazy loading not used as arff file is checked.
         dataset = openml.datasets.get_dataset(dataset_id, download_data=True)
         decoder = arff.ArffDecoder()
