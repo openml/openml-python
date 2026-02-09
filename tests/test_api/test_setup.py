1b63cb1f610600ccdc8685161dff969b6ddc89fa
Eman Abdelhaleem <emanabdelhaleem4@gmail.com>
2026-02-09 06:15:09 +0200

    updated api tests to use http_client, corrected exists tests

--- tests/test_api/test_setup.py
+++ tests/test_api/test_setup.py
@@ -14,7 +14,9 @@ from openml.testing import TestBase
 from openml._api.resources.setup import SetupV1API, SetupV2API
 from openml.setups.setup import OpenMLSetup
 from openml.testing import TestAPIBase
+from openml.enums import APIVersion
 from openml._api.resources.base.fallback import FallbackProxy
+from openml.exceptions import OpenMLNotSupportedError
 
 def get_sentinel():
     # Create a unique prefix for the flow. Necessary because the flow is
@@ -32,7 +34,8 @@ class TestSetupV1(TestAPIBase):
   
     def setUp(self) -> None:    
         super().setUp() 
-        self.resource = SetupV1API(self.http_client)
+        http_client = self.http_clients[APIVersion.V1]
+        self.resource = SetupV1API(http_client)
         self.extension = SklearnExtension()
   
     @pytest.mark.uses_test_server()
@@ -65,10 +68,11 @@ class TestSetupV1(TestAPIBase):
         TestBase._mark_entity_for_removal("flow", flow.flow_id, flow.name)
         TestBase.logger.info(f"collected from {__file__.split('/')[-1]}: {flow.flow_id}")
 
+        openml_param_settings = flow.extension.obtain_parameter_values(flow)
         # although the flow exists (created as of previous statement),
         # we can be sure there are no setups (yet) as it was just created
         # and hasn't been ran
-        setup_id = openml.setups.setup_exists(flow)
+        setup_id = self.resource.exists(flow, openml_param_settings)
         assert not setup_id
     
     @pytest.mark.sklearn()
@@ -81,9 +85,10 @@ class TestSetupV1(TestAPIBase):
         TestBase._mark_entity_for_removal("flow", flow.flow_id, flow.name)
         TestBase.logger.info(f"collected from {__file__.split('/')[-1]}: {flow.flow_id}")
 
+        openml_param_settings = flow.extension.obtain_parameter_values(flow)
         # although the flow exists, we can be sure there are no
         # setups (yet) as it hasn't been ran
-        setup_id = openml.setups.setup_exists(flow)
+        setup_id = self.resource.exists(flow, openml_param_settings)
         assert not setup_id
 
         # now run the flow on an easy task:
@@ -96,9 +101,8 @@ class TestSetupV1(TestAPIBase):
         TestBase.logger.info(f"collected from {__file__.split('/')[-1]}: {run.run_id}")
         # download the run, as it contains the right setup id
         run = openml.runs.get_run(run.run_id)
-
         # execute the function we are interested in
-        setup_id = openml.setups.setup_exists(flow)
+        setup_id = self.resource.exists(flow, openml_param_settings)
         assert setup_id == run.setup_id
 
 
@@ -109,53 +113,24 @@ class TestSetupV2(TestAPIBase):
   
     def setUp(self) -> None:    
         super().setUp() 
-        self.client = self._get_http_client(
-            server="http://localhost:8001/",
-            base_url="",
-            api_key="",
-            timeout_seconds=self.timeout_seconds,
-            retries=self.retries,
-            retry_policy=self.retry_policy,
-            cache=self.cache,
-        )
-        self.resource = SetupV2API(self.client)
-
-
-class TestSetupsCombined(TestAPIBase):
-    def setUp(self):
-        super().setUp()
-        self.v1_client = self.http_client
-        self.v2_client = self._get_http_client(
-            server="http://localhost:8001/",
-            base_url="",
-            api_key="",
-            timeout_seconds=self.timeout_seconds,
-            retries=self.retries,
-            retry_policy=self.retry_policy,
-            cache=self.cache,
-        )
-        self.resource_v1 = SetupV1API(self.v1_client)
-        self.resource_v2 = SetupV2API(self.v2_client)
-        self.resource_fallback = FallbackProxy(self.resource_v2, self.resource_v1)
+        http_client = self.http_clients[APIVersion.V2]
+        self.resource = SetupV2API(http_client)
         self.extension = SklearnExtension()
 
     @pytest.mark.uses_test_server()
-    def test_list_fallback(self):
-        output_fallback = self.resource_fallback.list(limit=10, offset=0)
-        assert isinstance(output_fallback, list)
-        assert len(output_fallback) > 0
-        assert all(isinstance(s, OpenMLSetup) for s in output_fallback)
-
+    def test_list(self):
+        with pytest.raises(OpenMLNotSupportedError):
+            self.resource.list(limit=10, offset=0)
+    
     @pytest.mark.uses_test_server()
-    def test_get_fallback(self):
-        setup_id = 1
-        output_fallback = self.resource_fallback.get(setup_id)
-        assert isinstance(output_fallback, OpenMLSetup)
-        assert output_fallback.setup_id == setup_id
+    def test_get(self):
+        with pytest.raises(OpenMLNotSupportedError):
+            setup_id = 1
+            self.resource.get(setup_id)
 
     @pytest.mark.sklearn()
     @pytest.mark.uses_test_server()
-    def test_exists_fallback(self):
+    def test_exists_nonexisting_setup(self):
         # first publish a non-existing flow
         sentinel = get_sentinel()
         # because of the sentinel, we can not use flows that contain subflows
@@ -166,5 +141,43 @@ class TestSetupsCombined(TestAPIBase):
         TestBase._mark_entity_for_removal("flow", flow.flow_id, flow.name)
         TestBase.logger.info(f"collected from {__file__.split('/')[-1]}: {flow.flow_id}")
 
-        output_fallback = openml.setups.setup_exists(flow)
-        assert not output_fallback  # Should be False for non-existing setup
+        openml_param_settings = flow.extension.obtain_parameter_values(flow)
+        # although the flow exists (created as of previous statement),
+        # we can be sure there are no setups (yet) as it was just created
+        # and hasn't been ran
+        with pytest.raises(OpenMLNotSupportedError):
+            self.resource.exists(flow, openml_param_settings)
+    
+    @pytest.mark.sklearn()
+    @pytest.mark.uses_test_server()
+    def test_exists_existing_setup(self):
+        flow = self.extension.model_to_flow(sklearn.naive_bayes.GaussianNB())
+        flow.name = f"{get_sentinel()}{flow.name}"
+        flow.publish()
+        TestBase._mark_entity_for_removal("flow", flow.flow_id, flow.name)
+        TestBase.logger.info(f"collected from {__file__.split('/')[-1]}: {flow.flow_id}")
+
+        openml_param_settings = flow.extension.obtain_parameter_values(flow)
+        # now run the flow on an easy task:
+        task = openml.tasks.get_task(115)  # diabetes; crossvalidation
+        run = openml.runs.run_flow_on_task(flow, task)
+        # spoof flow id, otherwise the sentinel is ignored
+        run.flow_id = flow.flow_id
+        run.publish()
+        TestBase._mark_entity_for_removal("run", run.run_id)
+        TestBase.logger.info(f"collected from {__file__.split('/')[-1]}: {run.run_id}")
+        # download the run, as it contains the right setup id
+        run = openml.runs.get_run(run.run_id)
+        # execute the function we are interested in
+
+        with pytest.raises(OpenMLNotSupportedError):
+            self.resource.exists(flow, openml_param_settings)
+
+
+class TestSetupsCombined(TestAPIBase):
+    def setUp(self):
+        super().setUp()
+        self.v1_client = self.http_clients[APIVersion.V1]
+        self.v2_client = self.http_clients[APIVersion.V2]
+        self.resource_v1 = SetupV1API(self.v1_client)
+        self.resource_v2 = SetupV2API(self.v2_client)
