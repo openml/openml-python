6ac1dfeeea3fb4aab1e7dc8d8cf6b6f0b627e9bd
geetu040 <raoarmaghanshakir040@gmail.com>
2026-02-19 03:14:09 +0500

    simplify _backend creation

--- openml/_api/setup/__init__.py
+++ openml/_api/setup/__init__.py
@@ -1,7 +1,8 @@
-from ._instance import _backend
 from .backend import APIBackend
 from .builder import APIBackendBuilder
 from .config import APIConfig, Config, ConnectionConfig
+
+_backend = APIBackend.get_instance()
 
 __all__ = [
     "APIBackend",
