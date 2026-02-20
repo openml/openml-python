fc839a6e6e680ed983974c9e30286f29d175efc9
geetu040 <raoarmaghanshakir040@gmail.com>
2026-02-20 19:16:53 +0500

    Revert "make delay functions static"
    This reverts commit 33b4ca0f103e0fa9d37368f6ee632d7e1f3217b9.

--- openml/_api/clients/http.py
+++ openml/_api/clients/http.py
@@ -24,8 +24,6 @@ from openml.exceptions import (
     OpenMLServerNoResult,
 )
 
-from .utils import human_delay, robot_delay
-
 
 class HTTPCache:
     """
@@ -230,7 +228,9 @@ class HTTPClient:
         self.retry_policy = retry_policy
         self.cache = cache
 
-        self.retry_func = human_delay if retry_policy == RetryPolicy.HUMAN else robot_delay
+        self.retry_func = (
+            self._human_delay if retry_policy == RetryPolicy.HUMAN else self._robot_delay
+        )
         self.headers: dict[str, str] = {"user-agent": f"openml-python/{__version__}"}
 
     def _parse_exception_response(
