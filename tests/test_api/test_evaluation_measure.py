97c21dd7aefbf9528c4a885e1050475807803707
Eman Abdelhaleem <emanabdelhaleem4@gmail.com>
2026-02-26 12:32:22 +0200

    update tests

--- tests/test_api/test_evaluation_measure.py
+++ tests/test_api/test_evaluation_measure.py
@@ -2,61 +2,35 @@
 from __future__ import annotations  
   
 import pytest    
-from openml._api.resources.evaluation_measure import EvaluationMeasureV1API, EvaluationMeasureV2API
-from openml.testing import TestAPIBase
-from openml.enums import APIVersion  
+from openml._api import EvaluationMeasureV1API, EvaluationMeasureV2API
 
-class TestEvaluationMeasureAPIBase(TestAPIBase):
-    resource: EvaluationMeasureV1API | EvaluationMeasureV2API
-    
-    def _list(self):
-        measures = self.resource.list()   
-        assert isinstance(measures, list) is True
-        assert all(isinstance(s, str) for s in measures) is True
-
-class TestEvaluationMeasureV1API(TestEvaluationMeasureAPIBase):  
-    """Tests for V1 XML API implementation of evaluation measures."""  
-  
-    _multiprocess_can_split_ = True  
-  
-    def setUp(self) -> None:    
-        super().setUp() 
-        http_client = self.http_clients[APIVersion.V1]
-        self.resource = EvaluationMeasureV1API(http_client)
-  
-    @pytest.mark.uses_test_server()
-    def test_list(self):
-        self._list()
+@pytest.fixture
+def evaluation_measure_v1(http_client_v1, minio_client) -> EvaluationMeasureV1API:
+    return EvaluationMeasureV1API(http=http_client_v1, minio=minio_client)
 
 
-class TestEvaluationMeasureV2API(TestEvaluationMeasureAPIBase): 
-    """Tests for V2 JSON API implementation of evaluation measures."""  
-  
-    _multiprocess_can_split_ = True  
-  
-    def setUp(self) -> None:    
-        super().setUp() 
-        http_client = self.http_clients[APIVersion.V2]
-        self.resource = EvaluationMeasureV2API(http_client)
-  
-    @pytest.mark.uses_test_server()
-    def test_list(self):
-        self._list()
+@pytest.fixture
+def evalutation_measure_v2(http_client_v2, minio_client) -> EvaluationMeasureV2API:
+    return EvaluationMeasureV2API(http=http_client_v2, minio=minio_client)
 
 
-class TestEvaluationMeasuresCombinedAPI(TestEvaluationMeasureAPIBase):
-    def setUp(self):
-        super().setUp()
-        self.v1_client = self.http_clients[APIVersion.V1]
-        self.v2_client = self.http_clients[APIVersion.V2]
-        self.resource_v1 = EvaluationMeasureV1API(self.v1_client)
-        self.resource_v2 = EvaluationMeasureV2API(self.v2_client)
+@pytest.mark.uses_test_server()    
+def test_v1_list(evaluation_measure_v1):
+    measures = evaluation_measure_v1.list()   
+    assert isinstance(measures, list) is True
+    assert all(isinstance(s, str) for s in measures) is True
 
-    @pytest.mark.uses_test_server()
-    def test_list_matches(self):
-        output_v1 = self.resource_v1.list()
-        output_v2 = self.resource_v2.list()
+@pytest.mark.uses_test_server()    
+def test_v2_list(evaluation_measure_v2):
+    measures = evaluation_measure_v2.list()   
+    assert isinstance(measures, list) is True
+    assert all(isinstance(s, str) for s in measures) is True
 
-        assert isinstance(output_v1, list)
-        assert isinstance(output_v2, list)
-        assert output_v1 == output_v2
+@pytest.mark.uses_test_server()
+def test_list_matches(evaluation_measure_v1,evalutation_measure_v2):
+    output_v1 = evaluation_measure_v1.list()
+    output_v2 = evalutation_measure_v2.list()
+
+    assert isinstance(output_v1, list)
+    assert isinstance(output_v2, list)
+    assert output_v1 == output_v2
