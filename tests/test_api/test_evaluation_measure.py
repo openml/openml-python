c6fc33abe3e809ef7cee58ba189b8e6a254d8331
geetu040 <raoarmaghanshakir040@gmail.com>
2026-03-23 13:46:49 +0500

    fix marker

--- tests/test_api/test_evaluation_measure.py
+++ tests/test_api/test_evaluation_measure.py
@@ -14,19 +14,19 @@ def evaluation_measure_v2(http_client_v2, minio_client) -> EvaluationMeasureV2AP
     return EvaluationMeasureV2API(http=http_client_v2, minio=minio_client)
 
 
-@pytest.mark.uses_test_server()    
+@pytest.mark.test_server()
 def test_v1_list(evaluation_measure_v1):
     measures = evaluation_measure_v1.list()   
     assert isinstance(measures, list) is True
     assert all(isinstance(s, str) for s in measures) is True
 
-@pytest.mark.uses_test_server()    
+@pytest.mark.test_server()
 def test_v2_list(evaluation_measure_v2):
     measures = evaluation_measure_v2.list()   
     assert isinstance(measures, list) is True
     assert all(isinstance(s, str) for s in measures) is True
 
-@pytest.mark.uses_test_server()
+@pytest.mark.test_server()
 def test_list_matches(evaluation_measure_v1,evaluation_measure_v2):
     output_v1 = evaluation_measure_v1.list()
     output_v2 = evaluation_measure_v2.list()
