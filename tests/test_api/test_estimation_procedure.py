f085f8f74d2c27f4df61bfad882c29c99f46028b
geetu040 <raoarmaghanshakir040@gmail.com>
2026-03-23 11:54:13 +0500

    fix marker: uses_test_server->test_server

--- tests/test_api/test_estimation_procedure.py
+++ tests/test_api/test_estimation_procedure.py
@@ -15,7 +15,7 @@ def estimation_procedure_v1(http_client_v1, minio_client) -> EstimationProcedure
 def estimation_procedure_v2(http_client_v2, minio_client) -> EstimationProcedureV2API:
     return EstimationProcedureV2API(http=http_client_v2, minio=minio_client)
 
-@pytest.mark.uses_test_server()
+@pytest.mark.test_server()
 def test_v1_list(estimation_procedure_v1):
     procedures = estimation_procedure_v1.list()
     
@@ -23,7 +23,7 @@ def test_v1_list(estimation_procedure_v1):
     assert len(procedures) > 0
     assert all(isinstance(p, str) for p in procedures)
 
-@pytest.mark.uses_test_server()   
+@pytest.mark.test_server()
 def test_v1_get_details(estimation_procedure_v1):
     details = estimation_procedure_v1._get_details()
     
@@ -35,7 +35,7 @@ def test_v1_get_details(estimation_procedure_v1):
     assert all("name" in d for d in details)
     assert all("task_type_id" in d for d in details)
 
-@pytest.mark.uses_test_server()
+@pytest.mark.test_server()
 def test_v2_list(estimation_procedure_v2):
     procedures = estimation_procedure_v2.list()
     
@@ -44,13 +44,13 @@ def test_v2_list(estimation_procedure_v2):
     assert all(isinstance(p, str) for p in procedures)
 
     
-@pytest.mark.uses_test_server()
+@pytest.mark.test_server()
 def test_v2_get_details(estimation_procedure_v2):
     with pytest.raises(OpenMLNotSupportedError):
         estimation_procedure_v2._get_details()
         
 
-@pytest.mark.uses_test_server()
+@pytest.mark.test_server()
 def test_list_matches(estimation_procedure_v1,estimation_procedure_v2):
     output_v1 = estimation_procedure_v1.list()
     output_v2 = estimation_procedure_v2.list()
