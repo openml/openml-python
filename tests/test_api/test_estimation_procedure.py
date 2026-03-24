3029936e8cf8f5ae5caa8f0845dfbfe761385531
geetu040 <raoarmaghanshakir040@gmail.com>
2026-03-24 17:58:45 +0500

    update tests

--- tests/test_api/test_estimation_procedure.py
+++ tests/test_api/test_estimation_procedure.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import pytest    
 from openml._api import EstimationProcedureV1API, EstimationProcedureV2API
 from openml.exceptions import OpenMLNotSupportedError
+from openml.estimation_procedures import OpenMLEstimationProcedure
 
 
 @pytest.fixture
@@ -18,46 +19,14 @@ def estimation_procedure_v2(http_client_v2, minio_client) -> EstimationProcedure
 
 @pytest.mark.test_server()
 def test_v1_list(estimation_procedure_v1):
-    procedures = estimation_procedure_v1.list()
-    
-    assert isinstance(procedures, list)
-    assert len(procedures) > 0
-    assert all(isinstance(p, str) for p in procedures)
-
-
-@pytest.mark.test_server()
-def test_v1_list(estimation_procedure_v1):
     details = estimation_procedure_v1.list()
     
     assert isinstance(details, list)
     assert len(details) > 0
-    assert all(isinstance(d, dict) for d in details)
-
-    assert all("id" in d for d in details)
-    assert all("name" in d for d in details)
-    assert all("task_type_id" in d for d in details)
+    assert all(isinstance(d, OpenMLEstimationProcedure) for d in details)
 
 
-@pytest.mark.test_server()
-def test_v2_list(estimation_procedure_v2):
-    procedures = estimation_procedure_v2.list()
-    
-    assert isinstance(procedures, list)
-    assert len(procedures) > 0
-    assert all(isinstance(p, str) for p in procedures)
-
-    
 @pytest.mark.test_server()
 def test_v2_list(estimation_procedure_v2):
     with pytest.raises(OpenMLNotSupportedError):
         estimation_procedure_v2.list()
-        
-
-@pytest.mark.test_server()
-def test_list_matches(estimation_procedure_v1,estimation_procedure_v2):
-    output_v1 = estimation_procedure_v1.list()
-    output_v2 = estimation_procedure_v2.list()
-
-    assert isinstance(output_v1, list)
-    assert isinstance(output_v2, list)
-    assert output_v1 == output_v2
