9575480b71d39222569f0a8bd9948d804e3a985e
geetu040 <raoarmaghanshakir040@gmail.com>
2026-03-24 18:22:01 +0500

    make private methods static

--- openml/_api/resources/setup.py
+++ openml/_api/resources/setup.py
@@ -18,41 +18,8 @@ if TYPE_CHECKING:
 class SetupV1API(ResourceV1API, SetupAPI):
     """V1 XML API implementation for setups."""
 
-    def list(
-        self,
-        limit: int,
-        offset: int,
-        *,
-        setup: Iterable[int] | None = None,
-        flow: int | None = None,
-        tag: str | None = None,
-    ) -> builtins.list[OpenMLSetup]:
-        """Perform API call `/setup/list/{filters}`
-
-        Parameters
-        ----------
-        The setup argument that is a list is separated from the single value
-        filters which are put into the kwargs.
-
-        limit : int
-        offset : int
-        setup : list(int), optional
-        flow : int, optional
-        tag : str, optional
-
-        Returns
-        -------
-        list
-            setups that match the filters, going from id to the OpenMLSetup object.
-        """
-        api_call = self._build_url(limit, offset, setup=setup, flow=flow, tag=tag)
-        setup_response = self._http.get(api_call)
-        xml_content = setup_response.text
-
-        return self._parse_list_xml(xml_content)
-
+    @staticmethod
     def _build_url(
-        self,
         limit: int,
         offset: int,
         *,
@@ -92,7 +59,8 @@ class SetupV1API(ResourceV1API, SetupAPI):
 
         return api_call
 
-    def _parse_list_xml(self, xml_content: str) -> builtins.list[OpenMLSetup]:
+    @staticmethod
+    def _parse_list_xml(xml_content: str) -> builtins.list[OpenMLSetup]:
         """Helper function to parse API calls which are lists of setups"""
         setups_dict = xmltodict.parse(xml_content, force_list=("oml:setup",))
         openml_uri = "http://openml.org/openml"
@@ -119,11 +87,12 @@ class SetupV1API(ResourceV1API, SetupAPI):
         )
 
         return [
-            self._create_setup({"oml:setup_parameters": setup_})
+            SetupV1API._create_setup({"oml:setup_parameters": setup_})
             for setup_ in setups_dict["oml:setups"]["oml:setup"]
         ]
 
-    def _create_setup(self, result_dict: dict) -> OpenMLSetup:
+    @staticmethod
+    def _create_setup(result_dict: dict) -> OpenMLSetup:
         """Turns an API xml result into a OpenMLSetup object (or dict)"""
         setup_id = int(result_dict["oml:setup_parameters"]["oml:setup_id"])
         flow_id = int(result_dict["oml:setup_parameters"]["oml:flow_id"])
@@ -138,14 +107,15 @@ class SetupV1API(ResourceV1API, SetupAPI):
             raise ValueError(
                 f"Expected None, list or dict, received something else: {type(xml_parameters)!s}",
             )
-            
+
         parameters = {
-            int(xml_parameter["oml:id"]): self._create_setup_parameter_from_xml(xml_parameter)
+            int(xml_parameter["oml:id"]): SetupV1API._create_setup_parameter_from_xml(xml_parameter)
             for xml_parameter in xml_parameters
         }
         return OpenMLSetup(setup_id, flow_id, parameters)
 
-    def _create_setup_parameter_from_xml(self, result_dict: dict[str, str]) -> OpenMLParameter:
+    @staticmethod
+    def _create_setup_parameter_from_xml(result_dict: dict[str, str]) -> OpenMLParameter:
         """Create an OpenMLParameter object or a dictionary from an API xml result."""
         return OpenMLParameter(
             input_id=int(result_dict["oml:id"]),
@@ -157,6 +127,67 @@ class SetupV1API(ResourceV1API, SetupAPI):
             default_value=result_dict["oml:default_value"],
             value=result_dict["oml:value"],
         )
+
+    @staticmethod
+    def _to_dict(
+        flow_id: int, openml_parameter_settings: builtins.list[dict[str, Any]]
+    ) -> OrderedDict:
+        """Convert a flow ID and a list of OpenML parameter settings to
+        a dictionary representation that can be serialized to XML.
+
+        Parameters
+        ----------
+        flow_id : int
+            ID of the flow.
+        openml_parameter_settings : list[dict[str, Any]]
+            A list of OpenML parameter settings.
+
+        Returns
+        -------
+        OrderedDict
+            A dictionary representation of the flow ID and parameter settings.
+        """
+        # for convenience, this function (ab)uses the run object.
+        xml: OrderedDict = OrderedDict()
+        xml["oml:run"] = OrderedDict()
+        xml["oml:run"]["@xmlns:oml"] = "http://openml.org/openml"
+        xml["oml:run"]["oml:flow_id"] = flow_id
+        xml["oml:run"]["oml:parameter_setting"] = openml_parameter_settings
+
+        return xml
+
+    def list(
+        self,
+        limit: int,
+        offset: int,
+        *,
+        setup: Iterable[int] | None = None,
+        flow: int | None = None,
+        tag: str | None = None,
+    ) -> builtins.list[OpenMLSetup]:
+        """Perform API call `/setup/list/{filters}`
+
+        Parameters
+        ----------
+        The setup argument that is a list is separated from the single value
+        filters which are put into the kwargs.
+
+        limit : int
+        offset : int
+        setup : list(int), optional
+        flow : int, optional
+        tag : str, optional
+
+        Returns
+        -------
+        list
+            setups that match the filters, going from id to the OpenMLSetup object.
+        """
+        api_call = SetupV1API._build_url(limit, offset, setup=setup, flow=flow, tag=tag)
+        setup_response = self._http.get(api_call)
+        xml_content = setup_response.text
+
+        return SetupV1API._parse_list_xml(xml_content)
 
     def get(self, setup_id: int) -> OpenMLSetup:
         """
@@ -178,7 +209,7 @@ class SetupV1API(ResourceV1API, SetupAPI):
         xml_content = setup_response.text
         result_dict = xmltodict.parse(xml_content)
 
-        return self._create_setup(result_dict)
+        return SetupV1API._create_setup(result_dict)
 
     def exists(
         self,
@@ -208,7 +239,9 @@ class SetupV1API(ResourceV1API, SetupAPI):
         """
         if flow.flow_id is None:
             raise ValueError("Flow must have a flow_id")
-        description = xmltodict.unparse(self._to_dict(flow.flow_id, param_settings), pretty=True)
+        description = xmltodict.unparse(
+            SetupV1API._to_dict(flow.flow_id, param_settings), pretty=True
+        )
         file_elements = {
             "description": ("description.arff", description),
         }
@@ -220,33 +253,6 @@ class SetupV1API(ResourceV1API, SetupAPI):
 
         setup_id = int(result_dict["oml:setup_exists"]["oml:id"])
         return setup_id if setup_id > 0 else False
-
-    def _to_dict(
-        self, flow_id: int, openml_parameter_settings: builtins.list[dict[str, Any]]
-    ) -> OrderedDict:
-        """Convert a flow ID and a list of OpenML parameter settings to
-        a dictionary representation that can be serialized to XML.
-
-        Parameters
-        ----------
-        flow_id : int
-            ID of the flow.
-        openml_parameter_settings : list[dict[str, Any]]
-            A list of OpenML parameter settings.
-
-        Returns
-        -------
-        OrderedDict
-            A dictionary representation of the flow ID and parameter settings.
-        """
-        # for convenience, this function (ab)uses the run object.
-        xml: OrderedDict = OrderedDict()
-        xml["oml:run"] = OrderedDict()
-        xml["oml:run"]["@xmlns:oml"] = "http://openml.org/openml"
-        xml["oml:run"]["oml:flow_id"] = flow_id
-        xml["oml:run"]["oml:parameter_setting"] = openml_parameter_settings
-
-        return xml
 
 
 class SetupV2API(ResourceV2API, SetupAPI):
