4719013adc6f77565fae724c3abd366a74a03765
JATAYU000 <shrivaths44kunju@gmail.com>
2026-03-27 14:47:15 +0530

    Revert class labels

--- tests/test_datasets/test_dataset_functions.py
+++ tests/test_datasets/test_dataset_functions.py
@@ -522,20 +522,18 @@ class TestOpenMLDataset(TestBase):
     def test__retrieve_class_labels(self):
         openml.config.set_root_cache_directory(self.static_cache_dir)
         labels = openml.datasets.get_dataset(2).retrieve_class_labels()
-        #TODO diff in prod
-        assert labels == ['nowin', 'won']
+        assert labels == ["1", "2", "3", "4", "5", "U"]
 
         labels = openml.datasets.get_dataset(2).retrieve_class_labels(
             target_name="product-type",
         )
-        #TODO no product-type in test/local
-        assert labels == None
+        assert labels == ["C", "H", "G"]
 
         # Test workaround for string-typed class labels
         custom_ds = openml.datasets.get_dataset(2)
         custom_ds.features[31].data_type = "string"
         labels = custom_ds.retrieve_class_labels(target_name=custom_ds.features[31].name)
-        assert labels == ["f","t"]
+        assert labels == ["COIL", "SHEET"]
 
     @pytest.mark.test_server()
     def test_upload_dataset_with_url(self):
